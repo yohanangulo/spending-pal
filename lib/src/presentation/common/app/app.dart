0dae37936a01d93e2bd2b2a45be940ff6a790d02
yohanangulo <anguloyohan98@gmail.com>
2025-05-08 04:44:47 -0400

    Move App widget

---
+++ lib/src/presentation/common/app/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'SpendingPal',
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Material App Bar')),
+        body: const Center(child: Text('Hello World')),
+      ),
+    );
+  }
+}
---
+++ lib/src/presentation/common/app/main_common.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:spending_pal/src/presentation/common/app/app.dart';
+
+Future<void> mainCommon() async {
+  runApp(App());
+}
