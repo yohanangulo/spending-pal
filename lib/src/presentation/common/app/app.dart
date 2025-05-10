5a39e2fcad107d74207101c45d21d801d9c63e62
yohanangulo <anguloyohan98@gmail.com>
2025-05-09 20:26:53 -0400

    Apply new format to app.dart

--- lib/src/presentation/common/app/app.dart
+++ lib/src/presentation/common/app/app.dart
@@ -6,6 +6,10 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(debugShowCheckedModeBanner: false, title: 'SpendingPal', routerConfig: router);
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      title: 'SpendingPal',
+      routerConfig: router,
+    );
   }
 }
