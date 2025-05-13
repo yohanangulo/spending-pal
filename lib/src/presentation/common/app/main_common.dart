5da5258b2eee8f0d20547e49e5326c605a23724f
yohanangulo <anguloyohan98@gmail.com>
2025-05-13 15:45:28 -0400

    Initial configuration

--- lib/src/presentation/common/app/main_common.dart
+++ lib/src/presentation/common/app/main_common.dart
@@ -1,8 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:spending_pal/src/config/config/config.dart';
+import 'package:spending_pal/src/config/config/firebase_options.dart';
 import 'package:spending_pal/src/presentation/common/app/app.dart';
 
 Future<void> mainCommon(Flavor flavor) async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Config.initialize(flavor);
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const App());
 }
