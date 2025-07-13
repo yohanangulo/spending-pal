03ebde4f00cf8724f2c9923517cfce82a844793f
yohanangulo <anguloyohan98@gmail.com>
2025-07-13 16:09:05 -0400

    Refactor dashboard screen

--- lib/src/presentation/screens/dashboard/dashboard_screen.dart
+++ lib/src/presentation/screens/dashboard/dashboard_screen.dart
@@ -1,17 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:spending_pal/src/presentation/common/resources/app_colors.dart';
+import 'package:spending_pal/src/presentation/common/resources/dimens.dart';
+import 'package:spending_pal/src/presentation/screens/dashboard/components/dashboard_chart.dart';
+import 'package:spending_pal/src/presentation/screens/dashboard/components/dashboard_header.dart';
+import 'package:spending_pal/src/presentation/screens/dashboard/components/expense_summary_cards.dart';
+import 'package:spending_pal/src/presentation/screens/dashboard/components/quick_actions.dart';
+import 'package:spending_pal/src/presentation/screens/dashboard/components/recent_transactions.dart';
 
 class DashboardScreen extends StatelessWidget {
   const DashboardScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
+    return Scaffold(
+      body: const SingleChildScrollView(
+        padding: EdgeInsets.all(Dimens.p4),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text('Dashboard'),
+            DashboardHeader(),
+            SizedBox(height: Dimens.p6),
+            ExpenseSummaryCards(),
+            SizedBox(height: Dimens.p6),
+            QuickActions(),
+            SizedBox(height: Dimens.p6),
+            RecentTransactions(),
+            SizedBox(height: Dimens.p6),
+            DashboardChart(),
           ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: AppColors.primary,
+        child: const Icon(
+          Icons.add,
+          color: Colors.white,
         ),
       ),
     );
