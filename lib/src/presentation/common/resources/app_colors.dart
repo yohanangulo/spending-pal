8faecd2bb810cdc9359719c08a46916130975fe7
yohanangulo <anguloyohan98@gmail.com>
2025-07-15 21:21:56 -0400

    Change primary color

--- lib/src/presentation/common/resources/app_colors.dart
+++ lib/src/presentation/common/resources/app_colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 abstract class AppColors {
-  static const Color primary = Color(0xFFb1ec6f);
+  static const Color primary = Color.fromARGB(255, 0, 221, 148);
   static const Color secondary = Color(0xFF1a1617);
   static const Color tertiary = Color(0xFFe9f3f0);
 }
