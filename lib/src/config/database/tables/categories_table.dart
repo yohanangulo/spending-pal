da04fcfed815e464ade96e88097369f148870f53
yohanangulo <anguloyohan98@gmail.com>
2025-09-27 21:34:21 -0400

    Change id type

--- lib/src/config/database/tables/categories_table.dart
+++ lib/src/config/database/tables/categories_table.dart
@@ -2,11 +2,14 @@ import 'package:drift/drift.dart';
 
 @DataClassName('CategoryDbDto')
 class CategoriesTable extends Table {
-  IntColumn get id => integer().autoIncrement()();
+  TextColumn get id => text()();
   TextColumn get name => text()();
   IntColumn get icon => integer()();
   IntColumn get color => integer()();
   TextColumn get userId => text()();
+
+  @override
+  Set<Column<Object>> get primaryKey => {id};
 
   @override
   List<Set<Column>> get uniqueKeys => [
