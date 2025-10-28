8670aadac3471494b9d8a773fe485c03176afcb9
yohanangulo <anguloyohan98@gmail.com>
2025-10-27 21:16:24 -0400

    Add brightness extension method

--- lib/src/config/extensions/brightness_extensions.dart
+++ lib/src/config/extensions/brightness_extensions.dart
@@ -3,4 +3,14 @@ part of 'extensions.dart';
 extension BrightnessExtensions on Brightness {
   bool get isDark => this == Brightness.dark;
   bool get isLight => this == Brightness.light;
+
+  T when<T>({
+    required T Function() dark,
+    required T Function() light,
+  }) {
+    return switch (this) {
+      Brightness.dark => dark(),
+      Brightness.light => light(),
+    };
+  }
 }
