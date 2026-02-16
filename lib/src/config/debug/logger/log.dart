b92fbefd2fb1289fc9732576f2d26d8dfc830cdf
yohanangulo <anguloyohan98@gmail.com>
2026-02-16 11:12:34 -0400

    Refactor logger traceStack

--- lib/src/config/debug/logger/log.dart
+++ lib/src/config/debug/logger/log.dart
@@ -10,23 +10,55 @@ class Log {
 
   final Logger _logger;
 
+  StackTrace _removeFirstFrame(StackTrace stackTrace) {
+    final lines = stackTrace.toString().split('\n');
+
+    if (lines.length <= 1) {
+      return stackTrace;
+    }
+
+    final cleaned = lines.sublist(1).join('\n');
+
+    return StackTrace.fromString(cleaned);
+  }
+
   void v(dynamic message, [dynamic error, StackTrace? stackTrace]) {
-    _logger.t(message, error: error, stackTrace: stackTrace);
+    _logger.t(
+      message,
+      error: error,
+      stackTrace: stackTrace ?? _removeFirstFrame(StackTrace.current),
+    );
   }
 
   void d(dynamic message, [dynamic error, StackTrace? stackTrace]) {
-    _logger.d(message, error: error, stackTrace: stackTrace);
+    _logger.d(
+      message,
+      error: error,
+      stackTrace: stackTrace ?? _removeFirstFrame(StackTrace.current),
+    );
   }
 
   void i(dynamic message, [dynamic error, StackTrace? stackTrace]) {
-    _logger.i(message, error: error, stackTrace: stackTrace);
+    _logger.i(
+      message,
+      error: error,
+      stackTrace: stackTrace ?? _removeFirstFrame(StackTrace.current),
+    );
   }
 
   void w(dynamic message, [dynamic error, StackTrace? stackTrace]) {
-    _logger.w(message, error: error, stackTrace: stackTrace);
+    _logger.w(
+      message,
+      error: error,
+      stackTrace: stackTrace ?? _removeFirstFrame(StackTrace.current),
+    );
   }
 
   void e(dynamic message, [dynamic error, StackTrace? stackTrace]) {
-    _logger.e(message, error: error, stackTrace: stackTrace);
+    _logger.e(
+      message,
+      error: error,
+      stackTrace: stackTrace ?? _removeFirstFrame(StackTrace.current),
+    );
   }
 }
