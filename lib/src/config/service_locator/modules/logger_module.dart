f918e0e2b29679a1ec0bdae05335b487b3cb072e
yohanangulo <anguloyohan98@gmail.com>
2026-02-17 20:16:36 -0400

    Update logger module configuration

--- lib/src/config/service_locator/modules/logger_module.dart
+++ lib/src/config/service_locator/modules/logger_module.dart
@@ -4,5 +4,20 @@ import 'package:logger/logger.dart';
 @module
 abstract class LoggerModule {
   @lazySingleton
-  Logger get logger => Logger(printer: PrettyPrinter(colors: false));
+  Logger get logger {
+    final levelPrefixes = {
+      Level.trace: '[T]',
+      Level.debug: '[🐛]',
+      Level.info: '[💡]',
+      Level.warning: '[⚠️]',
+      Level.error: '[⛔]',
+      Level.fatal: '[🔥]',
+    };
+
+    for (final level in SimplePrinter.levelPrefixes.entries) {
+      SimplePrinter.levelPrefixes[level.key] = levelPrefixes[level.key]!;
+    }
+
+    return Logger(printer: SimplePrinter(colors: false));
+  }
 }
