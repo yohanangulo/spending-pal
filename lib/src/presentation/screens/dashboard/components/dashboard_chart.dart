5a3de798c23c2549955249815c0f3301c6e682d0
yohanangulo <anguloyohan98@gmail.com>
2026-02-26 11:37:27 -0400

    Reorder weekly chart and add barchar animation

--- lib/src/presentation/screens/dashboard/components/dashboard_chart.dart
+++ lib/src/presentation/screens/dashboard/components/dashboard_chart.dart
@@ -1,5 +1,6 @@
 import 'dart:math' as math;
 
+import 'package:animate_do/animate_do.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -78,7 +79,7 @@ class DashboardChart extends StatelessWidget {
           ],
         );
       },
-    );
+    ).fadeInUp();
   }
 }
 
